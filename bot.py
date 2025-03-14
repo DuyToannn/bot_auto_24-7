11cd71c9537a0ec9d50760f94384a5b3a4e4825b
Duy Toan <toanpmo123@gmail.com>
2025-03-14 10:19:32 +0700

    fix service

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 import requests
 import time
 import json
